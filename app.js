7d43675e4b63040f3243d0723efe75e33deea30b
falloumacbook <falloumacbook@gmail.com>
2024-04-05 14:43:13 +1200

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('.container')
+const text = document.querySelector('#text')
+
+const totalTime = 7500
+const breathTime = (totalTime / 5) * 2
+const holdTime = totalTime / 5
+
+breathAnimation()
+
+function breathAnimation () {
+  text.innerHTML = 'Breath In!'
+  container.className = 'container grow'
+
+  setTimeout(() => {
+    text.innerText = 'Hold your breath!'
+
+    setTimeout(() => {
+      text.innerText = 'Breath Out!'
+      container.className = 'container shrink'
+    }, holdTime)
+  }, breathTime)
+}
+
+setInterval(breathAnimation, totalTime)
